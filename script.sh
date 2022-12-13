ce9de719384d9e6214ebc6f4e3c233bfa333a635
mili1211 <milica.djajic@hotmail.com>
2022-12-13 09:42:46 +0000

    minor changes

--- script.sh
+++ script.sh
@@ -1,16 +1,27 @@
+echo "Installing Ansible"
+
 # Update the package manager
-sudo apt-get update
+sudo apt update
+
 #Install all relevant dependencies on which Ansible will rely on to install correctly: 
-sudo apt-get install python3 python3-pip git libffi-dev libssl-dev -y 
+sudo apt install python3 python3-pip git libffi-dev libssl-dev -y
+
 #Install ansible : 
 python3 -m pip install --user ansible
+
+
+echo "Ansible core version"
 #Check version of ansible core:
 ansible --version
+
+echo "Ansible package version"
 #Check version of ansible package:
 python3 -m pip show ansible
+
 #Adding a directory to PATH:
 export PATH="/home/milica/.local/bin:$PATH"
-#Create the ansible configuration file
-sudo mkdir /etc/ansible
-ansible-config init --disabled -t all > ansible.cfg
-sudo mv ansible.cfg /etc/ansible
+
+#Create the ansible configuration file in case you don't have it
+#sudo mkdir /etc/ansible
+#ansible-config init --disabled -t all > ansible.cfg
+#sudo mv ansible.cfg /etc/ansible
